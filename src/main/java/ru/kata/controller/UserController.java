9af99360aff8d107709e44377f23b6ae1411805c
Aleksey Bershov <bershov.aleksey@bk.ru>
2025-03-31 22:32:52 +0300

    redi? not realy

--- src/main/java/ru/kata/controller/UserController.java
+++ src/main/java/ru/kata/controller/UserController.java
@@ -8,9 +8,10 @@ import ru.kata.model.User;
 import ru.kata.service.UserService;
 
 
+
 @Controller
 public class UserController {
-    private final UserService userService;
+    private UserService userService;
 
     @Autowired
     public UserController(UserService userService) {
@@ -23,79 +24,37 @@ public class UserController {
         return "user/index";
     }
 
+
     @GetMapping("/new")
     public String newUser(ModelMap m) {
         m.addAttribute("user", new User());
         return "user/new";
     }
 
-    @PostMapping("/new") // Создание нового пользователя
+    @PostMapping
     public String createUser(@ModelAttribute("user") User user) {
         userService.add(user);
         return "redirect:/";
     }
 
-    @GetMapping("/edit")
-    public String edit(ModelMap m, @RequestParam("id") int id) {
+    @GetMapping("/{id}/edit")
+    public String edit(ModelMap m,  @PathVariable("id") int id ) {
         m.addAttribute("user", userService.showUser(id));
         return "user/edit";
+
     }
 
-    @PostMapping("/update")
-    public String update(@ModelAttribute("user") User user, @RequestParam("id") int id) {
+    @PatchMapping(value = "/{id}")
+    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
         userService.update(id, user);
         return "redirect:/";
     }
 
-    @PostMapping("/delete")
-    public String delete(@RequestParam("id") int id) {
+    @DeleteMapping(value = "/{id}")
+    public String delete(@PathVariable("id") int id) {
         userService.remove(id);
         return "redirect:/";
     }
-}
 
-//@Controller
-//public class UserController {
-//    private final UserService userService;
-//
-//    @Autowired
-//    public UserController(UserService userService) {
-//        this.userService = userService;
-//    }
-//
-//    @GetMapping("/")
-//    public String showAll(ModelMap m) {
-//        m.addAttribute("users", userService.users());
-//        return "user/index";
-//    }
-//
-//    @GetMapping("/new")
-//    public String newUser(ModelMap m) {
-//        m.addAttribute("user", new User());
-//        return "user/new";
-//    }
-//
-//    @PostMapping("/new") // Убедитесь, что путь соответствует вашему маршруту
-//    public String createUser(@ModelAttribute("user") User user) {
-//        userService.add(user);
-//        return "redirect:/";
-//    }
-//
-//    @GetMapping("/edit")
-//    public String edit(ModelMap m, @RequestParam("id") int id) {
-//        m.addAttribute("user", userService.showUser(id));
-//        return "user/edit";
-//    }
-//
-//    @PatchMapping("/update")
-//    public String update(@ModelAttribute("user") User user, @RequestParam("id") int id) {
-//        userService.update(id, user);
-//        return "redirect:/";
-//    }
-//
-//    @DeleteMapping("/delete")
-//    public String delete(@RequestParam("id") int id) {
-//        userService.remove(id);
-//        return "redirect:/";
-//    }
-//}
+
+}
